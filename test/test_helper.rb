2798f791886c874ee63059549da9eabb75de416b
Patrick McKenzie <patrick@kalzumeus.com>
2011-01-25 22:27:51 +0900

    Allowing tests to run with Rails 3, via rake test:plugins PLUGIN=abingo and also through Netbeans."

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,30 @@
 require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
+require 'rails'
+
+if Rails::VERSION::MAJOR.to_i >= 3
+  require 'rails/all'
+  require 'test/unit'
+
+  require 'active_support'
+  require 'active_support/railtie'
+  require 'active_support/core_ext'
+  require 'active_support/test_case'
+
+  require 'action_controller'
+  require 'action_controller/caching'
+  require 'active_record'
+  require 'active_record/base'
+  
+  require 'rails'
+  require 'rails/application'
+
+  require 'rails/railtie'
+
+  require ::File.expand_path('../../../../../config/environment',  __FILE__)
+  Rails3test::Application.initialize! 
+else
+  #Rails 2 testing
+  require 'active_support'
+  require 'active_support/test_case'
+end
+
